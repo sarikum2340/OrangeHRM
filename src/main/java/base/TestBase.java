018a107ef4a7f2932309ba0f6a054c1d1df83cbe
sarikum1 <sarikum1@in.ibm.com>
2020-09-23 10:34:40 +0530

    Pom

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
-	public static WebDriver driver;
-	public static Properties prop;
+	protected static WebDriver driver;
+	protected static Properties prop;
 
-	private static final String PROPERTIES_FILE_PATH = "./OrangeHRM.properties";
-	private static final String PROPERTIES_FILE_URL = "URL";
-	private static final String CHROME_PATH = "chrome";
-	private static final String USERNAME = "username";
-	private static final String PASSWORD = "password";
+	protected static final String PROPERTIES_FILE_PATH = "./OrangeHRM.properties";
+	protected static final String PROPERTIES_FILE_URL = "URL";
+	protected static final String CHROME_PATH = "chrome";
+	protected static final String USERNAME = "username";
+	protected static final String PASSWORD = "password";
 
 	public TestBase() {
 
